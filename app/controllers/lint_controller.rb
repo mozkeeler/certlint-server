c16547472fe5b19f76559881d3a0c39a6d6d723b
Dana Keeler <dkeeler@mozilla.com>
2016-01-29 13:51:27 -0800

    exception handling, ui touchups

--- app/controllers/lint_controller.rb
+++ app/controllers/lint_controller.rb
@@ -34,35 +34,46 @@ end
 
 class LintController < ApplicationController
   def lint
+    @errors = []
     @chain = {}
     if params[:lint].has_key?('host') and params[:lint][:host].length > 0
-      uri = URI(params[:lint][:host])
-      socket = TCPSocket.new uri.host, uri.port
-      sslcontext = OpenSSL::SSL::SSLContext.new
-      sslsocket = OpenSSL::SSL::SSLSocket.new socket, sslcontext
-      sslsocket.connect
-      sslsocket.peer_cert_chain.each do |cert|
-        der = cert.to_der()
-        messages = CertLint::CABLint.lint(der)
-        id = certToIdentifier(cert)
-        @chain[id] = {}
-        organizeMessages(@chain[id], messages)
+      begin
+        @host = params[:lint][:host]
+        uri = URI(@host)
+        socket = TCPSocket.new uri.host, uri.port
+        sslcontext = OpenSSL::SSL::SSLContext.new
+        sslsocket = OpenSSL::SSL::SSLSocket.new socket, sslcontext
+        sslsocket.connect
+        sslsocket.peer_cert_chain.each do |cert|
+          der = cert.to_der()
+          messages = CertLint::CABLint.lint(der)
+          id = certToIdentifier(cert)
+          @chain[id] = {}
+          organizeMessages(@chain[id], messages)
+        end
+      rescue Exception => e
+        @errors << e.message
       end
     end
 
     if params[:lint].has_key?('root')
-      root = params[:lint][:root].read
-      if root.include? '-BEGIN CERTIFICATE-'
-        messages, der = CertLint::PEMLint.lint(root, 'CERTIFICATE')
-      else
-        messages = []
-        der = root
+      begin
+        @root = params[:lint][:root]
+        rootData = params[:lint][:root].read
+        if rootData.include? '-BEGIN CERTIFICATE-'
+          messages, der = CertLint::PEMLint.lint(rootData, 'CERTIFICATE')
+        else
+          messages = []
+          der = rootData
+        end
+        messages += CertLint::CABLint.lint(der)
+        x509Cert = OpenSSL::X509::Certificate.new der
+        id = certToIdentifier(x509Cert)
+        @chain[id] = {}
+        organizeMessages(@chain[id], messages)
+      rescue Exception => e
+        @errors << e.message
       end
-      messages += CertLint::CABLint.lint(der)
-      x509Cert = OpenSSL::X509::Certificate.new der
-      id = certToIdentifier(x509Cert)
-      @chain[id] = {}
-      organizeMessages(@chain[id], messages)
     end
   end
 end
