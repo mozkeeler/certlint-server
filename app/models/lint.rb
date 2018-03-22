7f66afa08213a2a149f09af52ef69d30720922a5
Dana Keeler <dkeeler@mozilla.com>
2018-03-22 14:54:02 -0700

    project deprecation

--- app/models/lint.rb
+++
@@ -1,2 +0,0 @@
-class Lint < ActiveRecord::Base
-end
--- test/controllers/lint_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LintControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
